f9f69ec653bd0e2afb33da8828ade72a90186393
parkie0517 <parkie0517@gmail.com>
2024-04-10 16:07:50 +0000

    Finalize visualization file

--- visualize_pc.py
+++ visualize_pc.py
@@ -1,9 +1,19 @@
+import numpy as np
 import open3d as o3d
+import os
+import sys
+
+def visualize_point_cloud(file_name):
+    # Load PCD file
+    pcd = o3d.io.read_point_cloud(file_name)
+
+    # Visualize the point cloud
+    o3d.visualization.draw_geometries([pcd])
 
 
-# Load the PCD file
-pcd_path = 'output.pcd'
-pcd = o3d.io.read_point_cloud(pcd_path)
-
-# Visualize the point cloud
-o3d.visualization.draw_geometries([pcd])
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python visualize_pc.py <filename>")
+    else:
+        filename = sys.argv[1]
+        visualize_point_cloud(filename)
