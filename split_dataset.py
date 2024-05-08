cb83d2294d2227fbff40e777c5fc20eeff705cdc
Heejun <parkie0517@gmail.com>
2024-05-08 04:49:43 +0000

    created a read, move code

--- split_dataset.py
+++ split_dataset.py
@@ -1,9 +1,19 @@
-count = 0
+import shutil # import shell utilities moudule
+import os
 
-with open('filename.txt', 'r') as file:
+# Define path!
+source_dir = '/root/datasets/NYU_Depth_V2/dataset/' # Define the source path of the file
+destination_dir = '/root/datasets/NYU_Depth_V2/dataset/train/' # Define the destination path
+text_path = '/root/datasets/NYU_Depth_V2/train.txt' # path of the train.txt
+
+
+with open(text_path, 'r') as file:
     # Read each line in the file one at a time
     for line in file:
-        # Print each line
-        print(line.strip())
-        count += 1
-print(count)
+        line = line.strip()
+        file_base = f'{line}.pth'
+        source_path = os.path.join(source_dir, file_base)
+        destination_path = os.path.join(destination_dir, file_base)
+        shutil.move(source_path, destination_path)
+        print(f'Done moving {file_base}')
+
