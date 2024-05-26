f654cd47b08a7f8e6b17c334537ce63d226ca95b
Heejun <parkie0517@gmail.com>
2024-05-26 07:53:42 +0000

    Add normal data

--- depth_2_pc_complete.py
+++ depth_2_pc_complete.py
@@ -13,6 +13,85 @@ cx_d = 3.1304475870804731e+02
 cy_d = 2.3844389626620386e+02
 
 
+import numpy as np
+
+def compute_normals(depth, width_downsample, height_downsample):
+    U, V = np.meshgrid(range(1, width_downsample + 1), range(1, height_downsample + 1))
+
+    x = ((U - cx_d) / fx_d) * depth
+    y = ((V - cy_d) / fy_d) * depth
+    z = depth
+
+    x = add_pad(x)
+    y = add_pad(y)
+    z = add_pad(z)
+
+    b_x_u_d = x[2:, 1:-1] - x[1:-1, 1:-1]
+    b_y_u_d = y[2:, 1:-1] - y[1:-1, 1:-1]
+    b_z_u_d = z[2:, 1:-1] - z[1:-1, 1:-1]
+
+    b_x_u_u = x[:-2, 1:-1] - x[1:-1, 1:-1]
+    b_y_u_u = y[:-2, 1:-1] - y[1:-1, 1:-1]
+    b_z_u_u = z[:-2, 1:-1] - z[1:-1, 1:-1]
+
+    a_x_r_r = x[1:-1, 2:] - x[1:-1, 1:-1]
+    a_y_r_r = y[1:-1, 2:] - y[1:-1, 1:-1]
+    a_z_r_r = z[1:-1, 2:] - z[1:-1, 1:-1]
+
+    a_x_r_l = x[1:-1, :-2] - x[1:-1, 1:-1]
+    a_y_r_l = y[1:-1, :-2] - y[1:-1, 1:-1]
+    a_z_r_l = z[1:-1, :-2] - z[1:-1, 1:-1]
+
+    normals_rd = np.zeros((height_downsample, width_downsample, 3))
+    normals_dl = np.zeros((height_downsample, width_downsample, 3))
+    normals_lu = np.zeros((height_downsample, width_downsample, 3))
+    normals_ur = np.zeros((height_downsample, width_downsample, 3))
+
+    normals_rd[:, :, 0] = a_y_r_r * b_z_u_d - a_z_r_r * b_y_u_d
+    normals_rd[:, :, 1] = a_z_r_r * b_x_u_d - a_x_r_r * b_z_u_d
+    normals_rd[:, :, 2] = a_x_r_r * b_y_u_d - a_y_r_r * b_x_u_d
+
+    norm_normals_rd = np.linalg.norm(normals_rd, axis=2)
+    normals_rd[:, :, 0] /= (norm_normals_rd + 1e-6)
+    normals_rd[:, :, 1] /= (norm_normals_rd + 1e-6)
+    normals_rd[:, :, 2] /= (norm_normals_rd + 1e-6)
+
+    normals_dl[:, :, 0] = b_z_u_d * a_y_r_l - b_y_u_d * a_z_r_l
+    normals_dl[:, :, 1] = b_x_u_d * a_z_r_l - b_z_u_d * a_x_r_l
+    normals_dl[:, :, 2] = b_y_u_d * a_x_r_l - b_x_u_d * a_y_r_l
+
+    norm_normals_dl = np.linalg.norm(normals_dl, axis=2)
+    normals_dl[:, :, 0] /= (norm_normals_dl + 1e-6)
+    normals_dl[:, :, 1] /= (norm_normals_dl + 1e-6)
+    normals_dl[:, :, 2] /= (norm_normals_dl + 1e-6)
+
+    normals_lu[:, :, 0] = a_y_r_l * b_z_u_u - a_z_r_l * b_y_u_u
+    normals_lu[:, :, 1] = a_z_r_l * b_x_u_u - a_x_r_l * b_z_u_u
+    normals_lu[:, :, 2] = a_x_r_l * b_y_u_u - a_y_r_l * b_x_u_u
+
+    norm_normals_lu = np.linalg.norm(normals_lu, axis=2)
+    normals_lu[:, :, 0] /= (norm_normals_lu + 1e-6)
+    normals_lu[:, :, 1] /= (norm_normals_lu + 1e-6)
+    normals_lu[:, :, 2] /= (norm_normals_lu + 1e-6)
+
+    normals_ur[:, :, 0] = b_z_u_u * a_y_r_r - b_y_u_u * a_z_r_r
+    normals_ur[:, :, 1] = b_x_u_u * a_z_r_r - b_z_u_u * a_x_r_r
+    normals_ur[:, :, 2] = b_y_u_u * a_x_r_r - b_x_u_u * a_y_r_r
+
+    norm_normals_ur = np.linalg.norm(normals_ur, axis=2)
+    normals_ur[:, :, 0] /= (norm_normals_ur + 1e-6)
+    normals_ur[:, :, 1] /= (norm_normals_ur + 1e-6)
+    normals_ur[:, :, 2] /= (norm_normals_ur + 1e-6)
+
+    normals = (normals_rd - normals_dl + normals_lu - normals_ur) / 4.0
+
+    return normals
+
+def add_pad(x):
+    x = np.pad(x, ((1, 1), (1, 1)), mode='constant')
+    return x
+
+
 
 def convert(file_base, input_path, output_path, scale):
     """1. Settings before we begin"""
@@ -78,8 +157,13 @@ def convert(file_base, input_path, output_path, scale):
     reshaped_rgb = rgb_image_array.reshape(rgb_height * rgb_width, rgb_channel)
 
 
-    """ 5. Save Data """
-    sample = {'coord':scaled_depth_PC, 'color':reshaped_rgb, 'semantic_gt':label_GT}
+    """ 7. Adding Normal Data """
+    normal = compute_normals(depth_image_array, depth_width, depth_height)
+    reshpaed_normal = normal.reshape(-1, 3) # reshape normal array
+
+
+    """ 6. Save Data """
+    sample = {'coord':scaled_depth_PC, 'color':reshaped_rgb, 'semantic_gt':label_GT, 'normal':reshpaed_normal}
 
     save_path = os.path.join(output_path, f'{file_base}.pth') 
     torch.save(sample, save_path)
